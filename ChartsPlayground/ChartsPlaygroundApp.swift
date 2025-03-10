6a9740a222e0b544fb699080794066a6d6060fc0
ilMike42 <ilmike_others@icloud.com>
2025-03-10 18:19:09 +0100

    Initial commit

---
+++ ChartsPlayground/ChartsPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsPlaygroundApp.swift
+//  ChartsPlayground
+//
+//  Created by Michael Planu on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ChartsPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartsPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChartsPlayground
+//
+//  Created by Michael Planu on 10/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            BarChart()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
